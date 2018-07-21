1a402c5b28d3361d645ef7dd4e6327d5436a556a
Jordy Guntur <jguntur@u.rochester.edu>
2018-07-20 21:06:42 -0400

    fix contact form

--- assets/contact-form/contact-form.php
+++ assets/contact-form/contact-form.php
@@ -19,7 +19,7 @@ $mail->Host = 'smtp.gmail.com';
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
 $mail->Port = 587;
-$mail->Username = 'sportstrata.contact@gmail.com';
+$mail->Username = 'jordy.guntur@gmail.com';
 $mail->Password = 'Zangetsu1!';
 
 echo $mail->ErrorInfo;
@@ -34,58 +34,51 @@ if( $_SERVER['REQUEST_METHOD'] == 'POST' ) {
 
 		$subject = 'Message from jordyguntur.com - ' . $name;
 
-		$botcheck = $_POST['quick-contact-form-botcheck'];
-
-		if( $botcheck == '' ) {
-
-			$mail->SetFrom( $email , $name );
-			$mail->AddReplyTo( $email , $name );
-			foreach( $toemails as $toemail ) {
-				$mail->AddAddress( $toemail['email'] , $toemail['name'] );
-			}
-			$mail->Subject = $subject;
-
-			$name = isset($name) ? "From: $name<br>" : '';
-			$email = isset($email) ? "Email: $email<br>" : '';
-			$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
-			$message = isset($message) ? "Message Body:<br> $message<br>" : '';
-
-			$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This e-mail was sent from a contact form on JordyGuntur: ' . $_SERVER['HTTP_REFERER'] : '';
-
-			$body = "$name $email $phone $message $referrer";
-
-			// Runs only when reCaptcha is present in the Contact Form
-			if( isset( $_POST['g-recaptcha-response'] ) ) {
-				$recaptcha_response = $_POST['g-recaptcha-response'];
-				$response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $recaptcha_response );
-
-				$g_response = json_decode( $response );
-
-				if ( $g_response->success !== true ) {
-					echo '{ "alert": "error", "message": "Captcha not Validated! Please Try Again." }';
-					die;
-				}
-			}
-
-			// Uncomment the following Lines of Code if you want to Force reCaptcha Validation
-
-			// if( !isset( $_POST['g-recaptcha-response'] ) ) {
-			// 	echo '{ "alert": "error", "message": "Captcha not Submitted! Please Try Again." }';
-			// 	die;
-			// }
-
-			$mail->MsgHTML( $body );
-			$sendEmail = $mail->Send();
-			header('Location: ../../contact.php?success=true');
-
-			if( $sendEmail == true ):
-				echo '{ "alert": "success", "message": "' . $message_success . '" }';
-			else:
-				echo '{ "alert": "error", "message": "Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
-			endif;
-		} else {
-			echo '{ "alert": "error", "message": "Bot <strong>Detected</strong>.! Clean yourself Botster.!" }';
+		$mail->SetFrom( $email , $name );
+		$mail->AddReplyTo( $email , $name );
+		foreach( $toemails as $toemail ) {
+			$mail->AddAddress( $toemail['email'] , $toemail['name'] );
 		}
+		$mail->Subject = $subject;
+
+		$name = isset($name) ? "From: $name<br>" : '';
+		$email = isset($email) ? "Email: $email<br>" : '';
+		$phone = isset($phone) ? "Phone: $phone<br><br>" : '';
+		$message = isset($message) ? "Message Body:<br> $message<br>" : '';
+
+		$referrer = $_SERVER['HTTP_REFERER'] ? '<br><br><br>This e-mail was sent from a contact form on JordyGuntur: ' . $_SERVER['HTTP_REFERER'] : '';
+
+		$body = "$name $email $phone $message $referrer";
+
+		// Runs only when reCaptcha is present in the Contact Form
+		// if( isset( $_POST['g-recaptcha-response'] ) ) {
+		// 	$recaptcha_response = $_POST['g-recaptcha-response'];
+		// 	$response = file_get_contents( "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret . "&response=" . $recaptcha_response );
+
+		// 	$g_response = json_decode( $response );
+
+		// 	if ( $g_response->success !== true ) {
+		// 		echo '{ "alert": "error", "message": "Captcha not Validated! Please Try Again." }';
+		// 		die;
+		// 	}
+		// }
+
+		// Uncomment the following Lines of Code if you want to Force reCaptcha Validation
+
+		// if( !isset( $_POST['g-recaptcha-response'] ) ) {
+		// 	echo '{ "alert": "error", "message": "Captcha not Submitted! Please Try Again." }';
+		// 	die;
+		// }
+
+		$mail->MsgHTML( $body );
+		$sendEmail = $mail->Send();
+		header('Location: ../../contact.php?success=true');
+
+		if( $sendEmail == true ):
+			echo '{ "alert": "success", "message": "' . $message_success . '" }';
+		else:
+			echo '{ "alert": "error", "message": "Email <strong>could not</strong> be sent due to some Unexpected Error. Please Try Again later.<br /><br /><strong>Reason:</strong><br />' . $mail->ErrorInfo . '" }';
+		endif;
 	} else {
 		echo '{ "alert": "error", "message": "Please <strong>Fill up</strong> all the Fields and Try Again." }';
 	}
